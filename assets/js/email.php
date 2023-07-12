cb417ae82a5ac2d00917f524a6da532c20270d50
Pietro Toledo <petetoledo123@gmail.com>
2023-07-11 19:41:35 -0500

    initial commit

--- assets/js/email.php
+++ assets/js/email.php
@@ -1,10 +1,10 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "petetoledo123@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@incandescent-heliotrope-2d5f8d.netlify.app/" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
